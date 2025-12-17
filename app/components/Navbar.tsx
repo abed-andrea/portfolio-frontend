7bedac83c8369c499511ca65761a6b5006148342
abed-andrea <andrea.abed03@gmail.com>
2025-12-16 23:49:02 -0800

    Add homepage placeholder sections

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar(){
             <div className="flex items-center justify-between px-8 text-[#818cf8]">
                 <a className="text-5xl font-semibold">Andrea Abed</a>
                 <nav className="flex gap-8 text-3xl">
+                    {/* the below hrefs will only work if you render navbar on a page that has id=about, id=projects, etc */}
                     <a href="#about">About</a>
                     <a href="#projects">Projects</a>
                     <a href="#contact">Contact</a>
